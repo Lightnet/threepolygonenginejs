c89ee770d154914005b8fe88c4749fc60fc24e09
Lightnet <darkneter@gmail.com>
2024-01-13 20:32:21 -0800

    clean up files.

--- assets/van_three.js
+++ assets/van_three.js
@@ -2,8 +2,7 @@
   Information:
     vanjs main client entry point
 */
-//import * as THREE from 'three';
-//import * as THREE from 'https://unpkg.com/three@0.157.0/build/three.module.js';
+
 import van from "https://cdn.jsdelivr.net/gh/vanjs-org/van/public/van-1.2.1.min.js";
 import {ThreeEL} from './triengine/triengine.js';
 van.add(document.body,ThreeEL())
